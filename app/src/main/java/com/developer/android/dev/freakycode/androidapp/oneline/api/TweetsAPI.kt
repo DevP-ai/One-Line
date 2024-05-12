5a67eaee121dc7736ef35333bddc0c9ce5641436
Devajit <patar1447@gmail.com>
2024-05-12 15:48:38 +0530

    Interface for API created and Retrofit Methods are define for end points

---
+++ app/src/main/java/com/developer/android/dev/freakycode/androidapp/oneline/api/TweetsAPI.kt
@@ -0,0 +1,17 @@
+package com.developer.android.dev.freakycode.androidapp.oneline.api
+
+import com.developer.android.dev.freakycode.androidapp.oneline.models.TweetListItem
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+
+interface TweetsAPI {
+
+    @GET("/v3/b/6640839de41b4d34e4f27d16?meta=false")
+    suspend fun getTweets(@Header("X-JSON-Path")category:String):Response<List<TweetListItem>>
+
+    @GET("/v3/b/6640839de41b4d34e4f27d16?meta=false")
+    @Headers("X-JSON-Path : tweets..category")
+    suspend fun getCategory():Response<List<String>>
+}
